180dd99a8d173cd6cb2e42a14c8169c287fd2648
@hexlet/cli <support@hexlet.io>
2022-09-26 11:15:32 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -5,5 +5,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
+public class PairedTag extends Tag {
 
+    private String bodyTag;
+    private List<Tag> singleTags;
+
+    public PairedTag(String tagName, Map<String, String> attribute, String bodyTag, List<Tag> singleTags) {
+        super(tagName, attribute);
+        this.bodyTag = bodyTag;
+        this.singleTags = singleTags;
+
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder(super.stringifyAttributes());
+
+        String singleTag = this.singleTags.stream().map(Tag::toString).collect(Collectors.joining());
+
+        return String.format("<%s%s>%s%s</%s>", tagName, stringifyAttributes(), singleTag, bodyTag, tagName);
+    }
+}
 // END
--- java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -3,23 +3,16 @@ package exercise;
 import java.util.Map;
 
 // BEGIN
-public class PairedTag extends Tag {
+public class SingleTag extends Tag {
 
-    private String bodyTag;
-    private List<Tag> singleTags;
-
-    public PairedTag(String tagName, Map<String, String> attribute, String bodyTag, List<Tag> singleTags) {
+    public SingleTag(String tagName, Map<String, String> attribute) {
         super(tagName, attribute);
-        this.bodyTag = bodyTag;
-        this.singleTags = singleTags;
 
     }
+
     public String toString() {
-        StringBuilder result = new StringBuilder(super.stringifyAttributes());
-
-        String singleTag = this.singleTags.stream().map(Tag::toString).collect(Collectors.joining());
-
-        return String.format("<%s%s>%s%s</%s>",tagName, stringifyAttributes(), singleTag, bodyTag, tagName);
+        return String.format("<%s%s>", tagName, stringifyAttributes());
     }
+
 }
 // END
