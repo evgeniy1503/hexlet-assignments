e253b5a1003f7980895a17c8fea848b2dccc96fc
@hexlet/cli <support@hexlet.io>
2022-12-24 17:10:29 +0300

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/model/User.java
@@ -0,0 +1,31 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // Имя пользователя, которое будет использоваться при аутентификации
+    private String username;
+
+    private String email;
+
+    // Поле, которое содержит пароль пользователя
+    // С этим паролем будет сравниваться пароль, введённый при аутентификации
+    private String password;
+}
